112bef8741cd260d6c5dd19004d6549552f88b21
Agatha Schneider <agatha.schneider@outlook.com>
2022-12-15 13:42:58 -0300

    day 1

--- 01-calorie-counting/src/main.rs
+++ 01-calorie-counting/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let content = fs::read_to_string(env::args().nth(1).expect("expected a file"))
         .expect("could not load the file");
 
-    let highest_calorie: usize = content
+    let mut elves: Vec<usize> = content
         .trim()
         .split("\n\n")
         .map(|elf| {
@@ -14,8 +14,10 @@ fn main() {
                 .map(|x| x.to_string().parse::<usize>().unwrap())
                 .sum()
         })
-        .max()
-        .unwrap();
+        .collect();
 
-    println!("{}", highest_calorie);
+    println!("{}", elves.iter().max().unwrap());
+
+    elves.sort();
+    println!("{}", elves.iter().rev().take(3).sum::<usize>());
 }
