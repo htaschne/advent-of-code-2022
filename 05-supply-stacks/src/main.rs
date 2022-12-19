11c129c39546a88c63e4d2c91bdd611d83bcc450
Agatha Schneider <agatha.schneider@outlook.com>
2022-12-19 14:32:40 -0300

    day 5 part 1

--- 05-supply-stacks/src/main.rs
+++ 05-supply-stacks/src/main.rs
@@ -1,13 +1,7 @@
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::env;
 use std::fs;
-
-fn show(stacks: &HashMap<&str, Vec<char>>) {
-    for (id, stack) in stacks {
-        println!("{}: {:?}", id, stack);
-    }
-
-}
 
 fn main() {
     let content = fs::read_to_string(env::args().nth(1).expect("expected a file"))
@@ -15,19 +9,17 @@ fn main() {
 
     let (piles, commands) = content.split_once("\n\n").unwrap();
 
-    let mut stacks: HashMap<&str, Vec<char>> = piles
+    let mut stacks: HashMap<&str, VecDeque<char>> = piles
         .lines()
         .map(|line| {
             let (id, items) = line.split_once(" ").unwrap();
-            (id, items.chars().collect::<Vec<char>>().clone())
+            (id, items.chars().collect::<VecDeque<char>>().clone())
         })
         .collect();
 
     let cmds = commands.to_string();
 
     for line in cmds.lines() {
-        show(&stacks);
-
         let cm: Vec<String> = line
             .split_whitespace()
             .map(|x| {
@@ -41,38 +33,32 @@ fn main() {
             .filter(|y| !y.is_empty())
             .collect();
 
-        println!("move: {:?}", cm);
-
         if let [mve, from, to] = &cm[..] {
             let mut mv = mve.parse::<u32>().unwrap();
 
             let f = stacks.get_mut(&from as &str);
             if let Some(f) = f {
                 let mut to_push: Vec<char> = Vec::new();
-                while mv > 0 { 
-                    to_push.push(f.remove(0));
+                while mv > 0 {
+                    to_push.push(f.pop_front().unwrap());
                     mv -= 1;
                 }
 
-                println!("PUSHING: {:?}", to_push);
                 let t = stacks.get_mut(&to as &str);
                 if let Some(t) = t {
                     for e in to_push.drain(..) {
-                        t.push(e);
+                        t.push_front(e);
                     }
                 }
-
             }
         }
-        // TODO: only print the last item in stack
-        // show(&stacks);
-        println!("----------")
     }
 
+    let mut sorted_keys: Vec<&&str> = stacks.keys().collect();
+    sorted_keys.sort();
 
-    // TODO: only print the last item in stack
-    for (id, stack) in &stacks {
-        println!("{}: {:?}", id, stack);
+    for e in sorted_keys {
+        print!("{}", stacks.get(e).unwrap().get(0).unwrap());
     }
-
+    println!("");
 }
