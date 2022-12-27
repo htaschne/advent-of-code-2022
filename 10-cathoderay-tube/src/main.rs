6b75af67687a79361f46ca198c47c1afebffa8c6
Agatha Schneider <agatha.schneider@outlook.com>
2022-12-27 00:55:18 -0300

    day 10 scratch

--- 10-cathoderay-tube/src/main.rs
+++ 10-cathoderay-tube/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::VecDeque;
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 
@@ -7,6 +8,7 @@ struct Cpu {
     instructions: VecDeque<String>,
     x: i32,
     cycles: usize,
+    to_add: HashMap<usize, i32>,
 }
 
 impl Cpu {
@@ -15,15 +17,33 @@ impl Cpu {
             instructions,
             x: 0,
             cycles: 0,
+            to_add: HashMap::new(),
         }
     }
 
     fn step(&mut self) {
-        let ins = self.instructions.pop_front().unwrap();
-        match ins {
-            _ => panic!("could not match instruction"),
-        }
+        let instruction = self.instructions.pop_front().unwrap();
+        let cmd: Vec<String> = instruction.split(" ").map(|cmd| cmd.to_string()).collect();
+        println!("{:?}", cmd);
+
+        match cmd.get(0).unwrap().as_str() {
+            "addx" => {
+                let index = self.cycles + 2;
+                let value = cmd.get(1).unwrap().parse::<i32>().unwrap();
+                println!("{} {}", index, value);
+                self.to_add.insert(index, value)
+            },
+            _ => Some(0),
+        };
+
+        self.cycles += 1;
         
+    }
+
+    fn run(&mut self) {
+        while self.instructions.len() > 0 {
+            self.step();
+        }
     }
 }
 
@@ -34,6 +54,7 @@ fn main() {
     let instructions: VecDeque<String> = content.lines().map(|line| line.to_string()).collect();
     let mut cpu = Cpu::new(instructions);
 
-    cpu.step();
-    println!("HELLO");
+    cpu.run();
+    println!("{}", cpu.cycles);
+    println!("{}", cpu.x);
 }
