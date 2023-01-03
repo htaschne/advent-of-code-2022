3ee0ed0f489886385299f57c0641251cc94113ab
Agatha Schneider <agatha.schneider@outlook.com>
2023-01-03 12:17:57 -0300

    day 21 part 1

--- 21-monkey-math/src/main.rs
+++ 21-monkey-math/src/main.rs
@@ -1,3 +1,47 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+
+fn find(root: &str, graph: &HashMap<&str, &str>) -> i64 {
+    match graph.get(root).unwrap().parse::<i64>() {
+        Ok(value) => value,
+        Err(_) => {
+            let operation = graph
+                .get(root)
+                .unwrap()
+                .split(" ")
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>();
+
+            match operation.get(1).unwrap().to_string().as_str() {
+                "+" => {
+                    find(operation.get(0).unwrap(), graph) + find(operation.get(2).unwrap(), graph)
+                }
+                "-" => {
+                    find(operation.get(0).unwrap(), graph) - find(operation.get(2).unwrap(), graph)
+                }
+                "*" => {
+                    find(operation.get(0).unwrap(), graph) * find(operation.get(2).unwrap(), graph)
+                }
+                "/" => {
+                    find(operation.get(0).unwrap(), graph) / find(operation.get(2).unwrap(), graph)
+                }
+                _ => panic!("could not mactch operation {}", operation.get(1).unwrap()),
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string(env::args().nth(1).expect("expected a file"))
+        .expect("could not load the file");
+
+    let graph = content
+        .lines()
+        .map(|line| line.split_once(": ").unwrap())
+        .collect::<HashMap<&str, &str>>();
+
+    let target = "root";
+    let result = find(target, &graph);
+    println!("{}", result);
 }
